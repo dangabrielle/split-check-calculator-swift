fd040c9a28a3ab6d8eb51194b85cb9aadaa47a3b
Gabrielle Dang <hoangabrielle@gmail.com>
2023-12-26 12:42:17 -1000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Gabrielle Dang on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
