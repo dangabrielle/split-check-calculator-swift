1aa38a626bc85de9381e29f8ae9632de08af04b0
dangabrielle <hoangabrielle@gmail.com>
2024-03-08 12:33:00 -1000

    add README and complete tab split calculation

---
+++ README.md
@@ -0,0 +1,2 @@
+#  WeSplit
+This application calculates a tip percentage from a check, and splits the check between x amount of people. This is useful for friends who enjoy dining out and prefer one overarching tab to later be divided per person.
--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,16 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grantTotal = checkAmount + tipValue
+        let amountPerPerson = grantTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var totalAmount: Double {
+        _ = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        
+        return checkAmount + tipValue
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    Picker("Number of People", selection: $numberOfPeople, content: {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    })
+//                    .pickerStyle(.navigationLink)
+                }
+                
+                Section ("How much tip do you want to leave?") {
+                    Picker("Tip Percentage", selection: $tipPercentage, content: {
+                        ForEach(0..<101) {
+                            Text($0, format: .percent)
+                        }
+                    })
+//                    .pickerStyle(.segmented)
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section ("Total amount"){
+                    Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Section ("Amount per person") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
